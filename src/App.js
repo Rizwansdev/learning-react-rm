05fb9a6c879195097d82b9aa5e66196e3eac8400
Rizwansdev <107733865+Rizwansdev@users.noreply.github.com>
2022-08-19 18:26:05 +0100

    Start of Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Switch, Route } from 
+'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Yooo</h1>
-    </div>
-  );
+    <>
+    <Router>
+      <Navbar />
+    <Switch>
+      <Route path='/' exact />
+    </Switch>
+    </Router>
+    
+    </>
+)
 }
 
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,17 @@
-import React, {useState} from 'react'
-
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function navbar() {
   return (
    <>
    <nav className="navbar">
     <div className="navbar-container">
-     
+     <Link to="/" className="navbar-logo">
+      TRVL <i class="gg-pokemon"></i>
+     </Link>
+     <div className="menu-icon">
+      
+     </div>
     </div>
    </nav>
    </>
